d5cd35b6f5f4e89f75cfb4e6faaf931cfbf6fe26
dayeonii <135932646+dayeonii@users.noreply.github.com>
2026-09-03 14:01:06 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 85 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Solution {
 
@@ -9,6 +11,16 @@ public class Solution {
 	static int arr[][];
 	static boolean visited[][];
 	static int delta[][] = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+
+	private static class Point {
+		int x;
+		int y;
+
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,36 +40,39 @@ public class Solution {
 				}
 			}
 
-			dfs(1, 1);
+			bfs(1, 1);
 
 			sb.append('#').append(tc).append(' ').append((finished == true) ? 1 : 0).append('\n');
 		}
 		System.out.print(sb);
 	}
 
-	private static void dfs(int r, int c) {
-		if (!isValid(r, c)) {
-			return;
-		}
-		if (arr[r][c]==3) {
-			finished = true;
-			return;
-		}
-
-		visited[r][c] = true;
+	private static void bfs(int r, int c) {
 		
-		for (int i = 0; i < 4; i++) {
-			int nr = r + delta[i][0];
-			int nc = c + delta[i][1];
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(r, c));
+		visited[r][c] = true;
 
-			if (arr[nr][nc] == 1) {
-				continue;
-			}
-			if (visited[nr][nc]==true) {
-				continue;
-			}
+		while (!queue.isEmpty()) {
 
-			dfs(nr, nc);
+			Point current = queue.poll();
+			int curR = current.x;
+			int curC = current.y;
+
+			if(arr[curR][curC]==3) {
+				finished = true;
+				return;
+			}
+			
+			for(int i=0; i<4; i++) {
+				int nr = curR + delta[i][0];
+				int nc = curC + delta[i][1];
+				if (!isValid(nr,nc) || arr[nr][nc]==1 || visited[nr][nc]==true) {
+					continue;
+				}
+				queue.offer(new Point(nr, nc));
+				visited[nr][nc]=true;
+			}
 		}
 	}
 
