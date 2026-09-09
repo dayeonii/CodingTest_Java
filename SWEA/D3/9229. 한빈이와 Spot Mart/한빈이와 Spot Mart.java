34e28a33875b9acec20521348cb0eceb320983fb
dayeonii <135932646+dayeonii@users.noreply.github.com>
2026-09-09 20:37:36 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 128 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,65 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int N, M;
+	static int maxWeight;
+	static int snacks[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 
+			snacks = new int[N];
 			st = new StringTokenizer(br.readLine());
-			int weights[] = new int[N];
 			for (int i = 0; i < N; i++) {
-				weights[i] = Integer.parseInt(st.nextToken());
+				snacks[i] = Integer.parseInt(st.nextToken());
 			}
 
-			int result = solvingProblem(weights, M);
+			maxWeight = -1;
+			perm(0, 0, 0);
 
-			sb.append('#').append(tc).append(' ').append(result).append('\n');
+			sb.append('#').append(tc).append(' ').append(maxWeight).append('\n');
 		}
 		System.out.print(sb);
 	}
 
-	private static int solvingProblem(int[] weights, int M) {
-		// 1. 정렬
-		Arrays.sort(weights);
+	private static void perm(int count, int curWeight, int idx) {
 
-		// 2. 가지치기
-		int start = 0;
-		int end = weights.length-1;
-		for (int i = 0; i < weights.length; i++) {
-			if (weights[i] >= M) {
-				end = i - 1;
+		// 종료 조건
+		if (count == 2) {
+			if (curWeight > maxWeight) {
+				maxWeight = curWeight;
 			}
+			return;
+		}
+		if (idx == snacks.length) {
+			return;
 		}
 
-		// 3. 양쪽에서 좁혀오기
-		int result = -1;
-		while (start < end) {
-			// 여유공간이 남으면 가벼운걸 더 늘려보고
-			if (weights[start] + weights[end] <= M) {
-				if (weights[start] + weights[end] > result) {
-					result = weights[start] + weights[end];
-				}
-				start++;
-			} else if (weights[start] + weights[end] > M) {
-				end--;
-			}
+		// 재귀
+		if (curWeight + snacks[idx] <= M) {
+			perm(count + 1, curWeight + snacks[idx], idx + 1); // 선택
 		}
-
-		return result;
+		perm(count, curWeight, idx + 1); // 안함
 	}
 
 }
